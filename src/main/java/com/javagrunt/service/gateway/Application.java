633c18a4a251248586901a081625941bde0340ec
dashaun <dashaun@dashaun.com>
2024-02-06 23:40:12 -0600

    move configs, add routes

--- src/main/java/com/javagrunt/service/gateway/Application.java
+++ src/main/java/com/javagrunt/service/gateway/Application.java
@@ -2,6 +2,7 @@ package com.javagrunt.service.gateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
@@ -10,7 +11,6 @@ import org.springframework.cloud.gateway.filter.factory.RetryGatewayFilterFactor
 import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -23,11 +23,7 @@ import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.G
 
 @SpringBootApplication
 public class Application {
-
-	private final String hello = "http://hello-service";
 	
-	private final String root = "https://javagrunt.github.io";
-
 	private final RequestCountingFilter scaler = new RequestCountingFilter();
 
 	public static void main(String[] args) {
@@ -40,14 +36,31 @@ public class Application {
 	}
 
 	@Bean
-	RouteLocator gateway(RouteLocatorBuilder rlb) {
+	RouteLocator gateway(RouteLocatorBuilder rlb,
+						 @Value("${hello-service.name}") String helloService,
+						 @Value("${youtube-listener.name}") String youtubeListener,
+						 @Value("${youtube-service.name}") String youtubeService,
+						 @Value("${namespace}") String namespace,
+						 @Value("${root.uri}") String root) {
 		return rlb.routes()
 				.route(r -> r.path("/hello-service/**")
 						.filters(f -> f.stripPrefix(1)
 							.filter(scaler))
-						.metadata("name", "hello-service")
-						.metadata("namespace", "dev-javagrunt-com")
-						.uri(hello))
+						.metadata("name", helloService)
+						.metadata("namespace", namespace)
+						.uri(String.format("http://%s", helloService)))
+				.route(r -> r.path("/youtube-service/**")
+						.filters(f -> f.stripPrefix(1)
+							.filter(scaler))
+						.metadata("name", youtubeService)
+						.metadata("namespace", namespace)
+						.uri(String.format("http://%s", youtubeService)))
+				.route(r -> r.path("/youtube-listener/**")
+						.filters(f -> f.stripPrefix(1)
+								.filter(scaler))
+						.metadata("name", youtubeListener)
+						.metadata("namespace", namespace)
+						.uri(String.format("http://%s", youtubeListener)))
 				.route(r -> r.path("/**").uri(root))
 				.build();
 	}
@@ -57,10 +70,8 @@ class RequestCountingFilter implements GatewayFilter, ScalerService {
 
 	Logger logger = LoggerFactory.getLogger(RequestCountingFilter.class);
 
-	private ConcurrentHashMap<String, AtomicInteger> counts = new ConcurrentHashMap<>();
-
-	private volatile boolean active = true;
-
+	private final ConcurrentHashMap<String, AtomicInteger> counts = new ConcurrentHashMap<>();
+	
 	private final GatewayFilter filter;
 
 	RequestCountingFilter() {
@@ -72,23 +83,24 @@ class RequestCountingFilter implements GatewayFilter, ScalerService {
 
 	@Override
 	public long getMetric(String name, String namespace) {
-		AtomicInteger val = counts.get(String.format("%s:%s", name, namespace));
-		logger.info("Returning count: " + val.get());
-		return val.get();
+		AtomicInteger count = getCount(name, namespace);
+		logger.info("Returning count: " + count.get());
+		return count.get();
 	}
 
 	@Override
 	public boolean isActive(String name, String namespace) {
-		AtomicInteger val = counts.get(String.format("%s:%s", name, namespace));
-		logger.info("Returning active: " + active + " and count: " + val.get());
-		return active && val.get() > 0;
+		AtomicInteger count = getCount(name, namespace);
+		logger.info("Returning active: " + (count.get() > 0));
+		return count.get() > 0;
 	}
 
 	@Override
 	public void setActive(String name, String namespace, boolean active) {
-		logger.info("Setting active to: " + active);
-		counts.get(String.format("%s:%s", name, namespace)).set(0);
-		this.active = active;
+		AtomicInteger count = getCount(name, namespace);
+		if(count.get() > 0){
+			count.set(0);
+		}
 	}
 
 	@Override
@@ -97,12 +109,8 @@ class RequestCountingFilter implements GatewayFilter, ScalerService {
         assert r != null;
         String name = r.getMetadata().get("name").toString();
 		String namespace = r.getMetadata().get("namespace").toString();
-		String key = String.format("%s:%s", name, namespace);
-		if(!counts.containsKey(key)) {
-			counts.put(key, new AtomicInteger(0));
-		}
-		counts.get(key).incrementAndGet();
-		AtomicInteger count = counts.get(key);
+		AtomicInteger count = getCount(name, namespace);
+		count.incrementAndGet();
 		return this.filter.filter(exchange, chain).doOnError(e -> {
 			if (count.get() > 0) {
 				logger.info("Decrementing count");
@@ -114,6 +122,14 @@ class RequestCountingFilter implements GatewayFilter, ScalerService {
 				count.decrementAndGet();
 			}
 		}));
+	}
+	
+	private AtomicInteger getCount(String name, String namespace) {
+		String key = String.format("%s:%s", name, namespace);
+		if(!counts.containsKey(key)) {
+			counts.put(key, new AtomicInteger(0));
+		}
+		return counts.get(key);
 	}
 
 }
